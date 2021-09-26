8a1b1c658753cf24fd769c89b13578eb5b192de0
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-26 21:16:35 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# smart
+# smart: Simple Maths for Art
+
+A collection of useful math functions like linear interpolation, clamping, and a modulo that respects the only sensible convention for negative numbers.
---
+++ src/smart.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace sma {
+
+template<typename T>
+T clamp_bot(T x, T min_value)
+{
+    return std::max(a, min_value);
+}
+
+template<typename T>
+T clamp_top(T x, T max_value)
+{
+    return std::min(a, max_value);
+}
+
+} // namespace sma
