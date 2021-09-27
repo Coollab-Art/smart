04590ffb2a1a61da3d519fc594920d4f03892ddf
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-27 14:25:42 +0200

    [test::clamp_bot] [test::clamp_top] Improve

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,34 +2,28 @@
 #include <doctest/doctest.h>
 #include <smart/smart.hpp>
 
-TEST_CASE("clamp_bot")
+TEST_CASE_TEMPLATE("clamp_bot", T, int, float, double)
 {
+    constexpr auto clamp_bot_5 = [](auto x)
     {
-        constexpr auto x = 10;
-        CHECK(smart::clamp_bot(x, 0) == x);
-        CHECK(smart::clamp_bot(x, 11) == 11);
-        CHECK(smart::clamp_bot(x, x) == x);
-    }
-    {
-        constexpr auto x = 10.f;
-        CHECK(smart::clamp_bot(x, 0.f) == x);
-        CHECK(smart::clamp_bot(x, 11.f) == 11.f);
-        CHECK(smart::clamp_bot(x, x) == x);
-    }
+        return smart::clamp_bot(x, T{5});
+    };
+    CHECK(clamp_bot_5(T{7}) == T{7});
+    CHECK(clamp_bot_5(T{6}) == T{6});
+    CHECK(clamp_bot_5(T{5}) == T{5});
+    CHECK(clamp_bot_5(T{4}) == T{5});
+    CHECK(clamp_bot_5(T{3}) == T{5});
 }
 
-TEST_CASE("clamp_top")
+TEST_CASE_TEMPLATE("clamp_top", T, int, float, double)
 {
+    constexpr auto clamp_top_5 = [](auto x)
     {
-        constexpr auto x = 3;
-        CHECK(smart::clamp_top(x, 10) == x);
-        CHECK(smart::clamp_top(x, 2) == 2);
-        CHECK(smart::clamp_top(x, x) == x);
-    }
-    {
-        constexpr auto x = 3.f;
-        CHECK(smart::clamp_top(x, 10.f) == x);
-        CHECK(smart::clamp_top(x, 2.f) == 2.f);
-        CHECK(smart::clamp_top(x, x) == x);
-    }
+        return smart::clamp_top(x, T{5});
+    };
+    CHECK(clamp_top_5(T{3}) == T{3});
+    CHECK(clamp_top_5(T{4}) == T{4});
+    CHECK(clamp_top_5(T{5}) == T{5});
+    CHECK(clamp_top_5(T{6}) == T{5});
+    CHECK(clamp_top_5(T{7}) == T{5});
 }
