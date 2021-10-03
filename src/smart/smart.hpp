68b42dbe32f1eda175c02beb4fbc93f1cc5bb3df
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-03 13:23:12 +0200

    Use lerp and midpoint from std

--- src/smart/smart.hpp
+++ src/smart/smart.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <cmath>
+#include <numeric>
+
 namespace smart
 {
+
 template<typename T>
 T keep_above(T x, T min_value)
 {
@@ -12,6 +16,18 @@ template<typename T>
 T keep_below(T x, T max_value)
 {
     return std::min(x, max_value);
+}
+
+template<typename T>
+T lerp(T a, T b, float t)
+{
+    return std::lerp(a, b, t);
+}
+
+template<typename T>
+T midpoint(T a, T b)
+{
+    return std::midpoint(a, b);
 }
 
 } // namespace smart
